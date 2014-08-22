5cdb034711671b8c21c7537ff9d953972bf66db1
psforbes <peter.s.forbes@gmail.com>
2014-08-22 13:56:01 -0500

    Completed assignment
    Commit of completed assignment 2 on Fri Aug 22 155pm

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions fulfill Programming Assignment 2. The purpose of the functions
+## is to save time by caching the values of time intensive calculations. In
+## this case, the inverse of a matrix is calculated and stored. 
 
-## Write a short comment describing this function
+## This function creates a matrix, then defines a series of functions
+## These functions set the value of the matrix, get the value of the matrix, 
+## set the value of the inverse of the matrix and get the value of the inverse 
+## of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv<- NULL
+    set<- function(y) {
+      x<<- y
+      inv<<- NULL
+    }
+    get<- function() x
+    setinv<- function(inverse) inv<<- inverse
+    getinv<- function() inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## The following function first checks to see if the inverse of the matrix
+## has been calculated. If is has, it skips the computation and returns the 
+## inverse of the matrix. If not, it proceeds with the calculation and sets
+## the value of the inverse matrix. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv<- x$getinv()
+        if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+        }
+        data<- x$get()
+        inv<- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
